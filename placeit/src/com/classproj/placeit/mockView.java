c1deb4440ed8d6b4e7248725cba0c90e6c3fa826
Charlie Wang <Charlie@Charlies-MacBook-Pro-5.local>
2014-02-12 02:10:18 -0800

    trying mock again

---
+++ placeit/src/com/classproj/placeit/mockView.java
@@ -0,0 +1,19 @@
+package com.classproj.placeit;
+
+import Models.PlaceIt;
+
+public class mockView implements iView{
+
+	@Override
+	public void addMarker(PlaceIt pc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeMarker(PlaceIt pc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
