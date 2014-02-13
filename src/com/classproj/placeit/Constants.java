d73a2783afd507a686de55c38300b8a26796fb7c
iteriani <fortrex@gmail.com>
2014-02-13 14:28:14 -0800

    premerge2

---
+++ src/com/classproj/placeit/Constants.java
@@ -0,0 +1,9 @@
+package com.classproj.placeit;
+
+import java.util.Calendar;
+
+public final class Constants {
+
+	final static int INTERVAL_TYPE = Calendar.SECOND;
+	final static int INTERVAL_NUMBER = 10;
+}
