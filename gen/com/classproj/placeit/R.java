6b419a4065abce8aa79427e6d857bc58e9179ad9
mgunarat <mgunarat@ucsd.edu>
2014-02-16 04:08:38 -0800

    mychange

--- gen/com/classproj/placeit/R.java
+++ gen/com/classproj/placeit/R.java
@@ -226,10 +226,10 @@ containing a value of this type.
         public static final int ic_plusone_tall_off_client=0x7f02001e;
     }
     public static final class id {
-        public static final int ScrollView01=0x7f05000c;
-        public static final int description=0x7f05000e;
+        public static final int ScrollView01=0x7f05000d;
+        public static final int description=0x7f05000f;
         public static final int drawer_layout=0x7f050005;
-        public static final int every=0x7f05000f;
+        public static final int every=0x7f050010;
         public static final int find=0x7f050007;
         public static final int hybrid=0x7f050004;
         public static final int left_drawer=0x7f05000a;
@@ -238,12 +238,13 @@ containing a value of this type.
         public static final int myBack=0x7f050006;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
-        public static final int numweeks=0x7f050010;
+        public static final int numweeks=0x7f050011;
+        public static final int right_drawer=0x7f05000b;
         public static final int satellite=0x7f050002;
         public static final int terrain=0x7f050003;
-        public static final int textView=0x7f05000b;
-        public static final int title=0x7f05000d;
-        public static final int weeks=0x7f050011;
+        public static final int textView=0x7f05000c;
+        public static final int title=0x7f05000e;
+        public static final int weeks=0x7f050012;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f070000;
