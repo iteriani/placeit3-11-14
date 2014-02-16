79f492298f507e920d1f35fab60f17bc4f370f57
iteriani <fortrex@gmail.com>
2014-02-16 00:53:28 -0800

    premerge

---
+++ src/Models/charlieMockView.java
@@ -0,0 +1,42 @@
+package Models;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.classproj.placeit.iView;
+import com.google.android.gms.maps.model.Marker;
+
+public class charlieMockView implements iView{
+
+	List<PlaceIt> mMarkers = new LinkedList<PlaceIt>();
+	
+	public charlieMockView (List<PlaceIt> userInput) {
+		mMarkers = userInput; 
+	}
+	
+	@Override
+	public void addMarker(PlaceIt pc) {
+		// TODO Auto-generated method stub
+		
+		mMarkers.add(pc);
+	}
+
+	@Override
+	public void removeMarker(PlaceIt pc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Marker getMarker(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void notifyUser(List<PlaceIt> placeits, String ControllerType) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
